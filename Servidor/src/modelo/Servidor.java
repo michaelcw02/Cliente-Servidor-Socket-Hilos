3debd11729fcec5d3e5b631843e0c4772714cd6c
michaelcw02 <michael.cw02@hotmail.com>
2016-10-12 17:08:58 -0600

    servidor

--- Servidor/src/modelo/Servidor.java
+++ Servidor/src/modelo/Servidor.java
@@ -5,15 +5,12 @@
  */
 package modelo;
 
-import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
 import java.io.PrintStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,52 +22,44 @@ public class Servidor {
 
     public Servidor() {
         listaClientes = new LinkedList<>();
+        clienteEscuchando = new EscuchaCliente(PUERTO_SALIDA);
     }
     public void agregarClientes(String ip) {
         listaClientes.add(new InetSocketAddress(ip, PUERTO_SALIDA));
     }
     public void ejecutar(String cmd, int cantidad) {
         
+        int cantPerClient = cantidad / listaClientes.size();
         cmd = CMD_INICIAL + cmd + " ";
         
-        ListIterator<InetSocketAddress> iter = listaClientes.listIterator();
-        String auxCmd;
-        for(int i = 1; i <= cantidad; i++) {
-            auxCmd = cmd + String.valueOf(i);
-            if(iter.hasNext()) {
-                InetSocketAddress client = iter.next();
-                messages(client, auxCmd);
-            }
-            else {
-                iter = listaClientes.listIterator();
-                i--;
-            }
+        int counter = 1;
+        for (InetSocketAddress cliente : listaClientes) {
+            try {
+                socket = new Socket(cliente.getAddress(), cliente.getPort());
+                sendMessage(cmd, cantPerClient, counter++);
+                readMessages();
+            } catch (Exception ex) {
+            }            
+        }
+        try {
+        } catch (Exception ex) {
         }
     }
     
-    private boolean messages(InetSocketAddress cliente, String str) {
+    private boolean sendMessage(String cmd, int cant, int number) {
         boolean ok = false;
-        String mensajeOriginal;
+        int start = (cant * number) - cant + 1;
+        int end = cant * number;
         
         try {
-            socket = new Socket(cliente.getAddress(), cliente.getPort());
-            
             outputData = new PrintStream(socket.getOutputStream());
-            inputData = new DataInputStream(socket.getInputStream());
-            
-            outputData.print(str);
-            Thread.sleep(1000);
-            mensajeOriginal = readMessages(socket);
-            System.out.println("> " + mensajeOriginal + "\n");
-            
-            /*
-            Thread.sleep(1000);
-            
-            if ((mensajeOriginal = inputData.readLine()) != null)
-                System.out.println("> " + mensajeOriginal + "\n");
-            */
+            String cmdToSend;
+            for(int i = start; i <= end; i++) {
+                cmdToSend = cmd + String.valueOf(i);
+                outputData.println(cmdToSend);
+            }
             ok = true;
-        } catch (Exception e) {    
+        } catch (Exception e) {
         }
         finally {
             try{
@@ -83,30 +72,27 @@ public class Servidor {
         return ok;
     }
     
-    private String readMessages(Socket clientSocket) {
-        int red = -1;
-        byte[] buffer = new byte[5 * 1024]; // a read buffer of 5KiB
-        byte[] redData;
-        StringBuilder clientData = new StringBuilder();
-        String redDataText;
-        try {
-            while ((red = clientSocket.getInputStream().read(buffer)) > -1) {
-                redData = new byte[red];
-                System.arraycopy(buffer, 0, redData, 0, red);
-                redDataText = new String(redData, "UTF-8"); // assumption that client sends data UTF-8 encoded
-                System.out.println("message part recieved:" + redDataText);
-                clientData.append(redDataText);
-            }
-        } catch (IOException ex) {
+    private void readMessages() {
+        
+        EscuchaCliente clienteEscuchando = new EscuchaCliente(PUERTO_SALIDA);
+        clienteEscuchando.start();
+         
+    }
+       
+    public void respuestas() {
+        
+        clienteEscuchando.stop();
+        for(String respuesta: clienteEscuchando.getResultados()) {
+                System.out.println(respuesta);
         }
-        return clientData.toString();
+        
+        
     }
     
     PrintStream outputData;
-    DataInputStream inputData;
     private Socket socket = null;
-    OutputStreamWriter osw = null;
     private List<InetSocketAddress> listaClientes = null;
+    private EscuchaCliente clienteEscuchando = null;
     private final int PUERTO_SALIDA = 8585;
     private final String CMD_INICIAL = "CALCULAR_";
                                       //CALCULAR_PRIMO
