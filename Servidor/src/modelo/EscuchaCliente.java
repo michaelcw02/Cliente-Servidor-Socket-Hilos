bf76b8091fce0da036eb49cca67998cebd71152c
michaelcw02 <michael.cw02@hotmail.com>
2016-10-12 17:09:06 -0600

    escuchacliente

--- Servidor/src/modelo/EscuchaCliente.java
+++ Servidor/src/modelo/EscuchaCliente.java
@@ -7,9 +7,11 @@ package modelo;
 
 import java.io.DataInputStream;
 import java.io.IOException;
-import java.net.InetSocketAddress;
+import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -17,31 +19,61 @@ import java.util.LinkedList;
  */
 public class EscuchaCliente extends Thread {
     
-    public EscuchaCliente(InetSocketAddress client) {
-        cliente = client;
+    public EscuchaCliente(int port) {
         resultados = new LinkedList<>();
+        activo = true;
+        PUERTO = port;
     }
     
     @Override
     public void run() {
+        
         try {
-            socket = new Socket(cliente.getAddress(), cliente.getPort());
-            dataInput = new DataInputStream(socket.getInputStream());
-            String mensaje;
-            while( (mensaje = dataInput.readLine()) != null) {
-                resultados.add(mensaje);
+            servidor = new ServerSocket(PUERTO+1);
+        } catch (Exception e) {
+            System.out.println("NO SE PUDO CONECTAR AL SERVIDOR..");
+            activo = false;
+        }
+        
+        System.out.println("SERVIDOR ESCUCHANDO..");
+        while (activo) {
+            try {
+
+                socket = servidor.accept();
+                System.out.println("SERVIDOR CONECTADO..");
+                dataInput = new DataInputStream(socket.getInputStream());
+                String mensaje;
+                while ((mensaje = dataInput.readLine()) != null) {
+                    System.out.println(">" + mensaje);
+                    resultados.add(mensaje);
+                }
+
+            } catch (Exception ex) {
+                resultados.add("ERROR ON CLIENT");
             }
-        } catch (IOException ex) {
-            resultados.add("ERROR ON CLIENT");
-        }        
+            try {
+                socket.close();
+            } catch (Exception ex) {
+            }
+        }
     }
     
     public LinkedList<String> getResultados() {
         return resultados;
     }
+
+    public void setActivo(boolean activo) {
+            this.activo = activo;
+        try {
+            socket.close();
+        } catch (IOException ex) {
+        }
+    }
     
-    private InetSocketAddress cliente;
+    boolean activo;
     private Socket socket;
+    private ServerSocket servidor;
     DataInputStream dataInput;
     LinkedList<String> resultados;
+    private int PUERTO;
 }
