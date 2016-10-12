57d9d093788d5c50ca4fd7e0f76cb9b2664929fc
michaelcw02 <michael.cw02@hotmail.com>
2016-10-12 07:03:29 -0600

    Added class Numero

---
+++ Cliente/src/modelo/Numero.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+/**
+ *
+ * @author Michael Chen W.
+ */
+public class Numero {
+    
+    private Numero() {
+        instancia = new Numero();
+    }
+    public static Numero getInstancia() {
+        if(instancia == null)
+            instancia = new Numero();
+        return instancia;
+    }
+    
+    public int calcularInverso(int numero) {
+        int num_inv = 0;
+        int div_entera = numero;
+        int resto_div = 0;
+        while (div_entera != 0) {
+            resto_div = div_entera % 10;
+            div_entera = div_entera / 10;
+            num_inv = num_inv * 10 + resto_div;
+        }
+        return num_inv;
+    }
+    
+    public boolean calcularPrimo(int numero) {
+        int contador = 2;
+        boolean primo = true;
+        while ((primo) && (contador != numero)) {
+            if (numero % contador == 0) {
+                primo = false;
+            }
+            contador++;
+        }
+        return primo;
+    }
+    
+    private static Numero instancia = null;
+}
