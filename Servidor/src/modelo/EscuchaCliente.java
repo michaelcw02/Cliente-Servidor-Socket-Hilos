9deb8e291fb3e5d5f262228a8b44324165027126
michaelcw02 <michael.cw02@hotmail.com>
2016-10-13 00:06:03 -0600

    changed to bufferstream

--- Servidor/src/modelo/EscuchaCliente.java
+++ Servidor/src/modelo/EscuchaCliente.java
@@ -5,8 +5,9 @@
  */
 package modelo;
 
-import java.io.DataInputStream;
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -37,9 +38,10 @@ public class EscuchaCliente extends Thread {
 
                 socket = servidor.accept();
                 System.out.println("SERVIDOR CONECTADO..");
-                dataInput = new DataInputStream(socket.getInputStream());
+                dataInput = new InputStreamReader(socket.getInputStream());
+                input = new BufferedReader(dataInput);
                 String mensaje;
-                while ((mensaje = dataInput.readLine()) != null) {
+                while ((mensaje = input.readLine()) != null) {
                     if (mensaje != " ") {
                         System.out.println(">" + mensaje);
                         resultados = resultados + (mensaje + "\n");
@@ -71,7 +73,8 @@ public class EscuchaCliente extends Thread {
     boolean activo;
     private Socket socket;
     private ServerSocket servidor;
-    DataInputStream dataInput;
+    InputStreamReader dataInput;
+    BufferedReader input;
     String resultados = "\n";
     private int PUERTO;
 }
